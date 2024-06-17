c6ba91090a4c2ce32049f5096a5c08d874adb18e
Halyna Danylko <danilkogallina@gmail.com>
2024-06-17 15:39:41 +0300

    replace APP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App/App';
+// import { App } from '';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
 // import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
